0bb18bb285c9034a4909c7983a4b7b625a0638c9
Ayush <ayush0999aryan@gmail.com>
2023-08-10 22:20:27 +0530

    First Messages

---
+++ Variables.php
@@ -0,0 +1,14 @@
+<?php
+
+// Variables = Variables are containers for storing information and it starts with $
+
+$name = "Ayush"; 
+$namE = "Captial Ayush";
+
+$income = 2000.8;
+
+echo "This is $name and his pocket money is $income  <br> ";
+
+echo " $name is the real gangsta and superhero ";
+
+?>
